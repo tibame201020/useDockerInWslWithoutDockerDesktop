a5f66f93ad498dc0562c221eea3ac4d38657fd5c
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-03-28 08:33:14 +0800

    Create install.sh

---
+++ minikube/install.sh
@@ -0,0 +1,11 @@
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+minikube start --driver=docker
+minikube status
